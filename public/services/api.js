7f52ca83eeeed3bd8a4f3506153dc9e57b723f75
Emmanuelfwd <eruizhurtado25fwd@gmail.com>
2025-09-02 15:26:58 -0600

    cambios services

--- public/services/api.js
+++ public/services/api.js
@@ -1,27 +1,13 @@
 export async function getPersonajes(status = 'all') {
-    let url = 'https://rickandmortyapi.com/api/character';
-
-    
-    if (status !== 'all') {
-        url += `?status=${status}`;
-    }
+    const url = `https://rickandmortyapi.com/api/character${status !== 'all' ? `?status=${status}` : ''}`;
 
     try {
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error('Error al obtener personajes');
-        }
-
+        const response = await fetch(url);
+        if (!response.ok) throw new Error('Error al obtener personajes');
         const data = await response.json();
-        return data.results; 
+        return data.results;
     } catch (error) {
-        console.error('Error en la API:', error);
+        console.error(error);
         throw error;
     }
 }
